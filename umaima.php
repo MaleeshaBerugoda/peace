023f09b56943244bfc01e94f9b70077740fdd21b
Maleesha Mithun Berugoda <88098373+awankaukussa@users.noreply.github.com>
2021-07-29 20:17:30 +0530

    Add files via upload

---
+++ umaima.php
@@ -0,0 +1,50 @@
+<?php
+
+$name=$_GET["name"];
+echo "Your Name is:-" . $name;
+$address =$_GET["address"];
+echo "<br>Your Address is:-".$address;
+$email =$_GET["email"];
+echo "<br>Your E-mail is:-".$email;
+$pnumber =$_GET["pnumber"];
+echo "<br>Your Phone Number:-".$pnumber;
+$name =$_GET["iq"];
+echo "<br>Your Iq:-".$name;
+$gender = $_GET["gender"]; 
+echo "<br>You are:- " . $gender;
+$date =$_GET["Proposeal"];
+echo "<br>Appointment Date:-".$date;
+$tatoos= $_GET["tatoos"];
+echo "<br>You Have No tatoos:-" .$tatoos;
+$older =$_GET["older"];
+echo "<br>You are older 2years more than my daughter:-".$older;
+$vehical =$_GET["vehical"];
+echo "<br>You Have a Vehical:-" . $vehical;
+$occupation =$_GET["occupation"];
+echo "<br>You work full time:-" . $occupation;
+$worth =$_GET["worth"];
+echo "<br>You have a rich background:-" . $worth;
+$location =$_GET["location"];
+echo "<br>Your meeting will be public place:-" . $location;
+$ppresuasion =$_GET["politicalpresuasion"];
+echo "<br>You Are:-" .$ppresuasion;
+$education =$_GET["education"];
+echo "<br>You are :-" . $education;
+$wdaughter =$_GET["wdaughter"];
+echo "<br>You need to:-" . $wdaughter;
+$contact =$_GET["contact"];
+echo "<br>Referances:-" . $contact;
+
+
+
+?>
+<html>
+    <head>
+        <title>Thankyou page</title>
+    <link rel="stylesheet" href="umaima.css">
+
+    </head>
+    <body>
+        <H1>Thankyou</H1>
+    </body>
+</html>
